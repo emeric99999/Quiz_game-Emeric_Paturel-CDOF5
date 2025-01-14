e40307c9d203ac6d2e6d004814f017571c271034
emeric99999 <emeric.paturel@gmail.com>
2025-01-14 15:44:22 +0100

    readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Quiz_game-Emeric_Paturel-CDOF5
-Quiz game in Python on gaming
+
+Ce projet est un jeu sous forme de quiz de culture générale 
+
+
+### Prérequis
+
+Installation de Python
+
+### Installation
+
+Cloner le repository 
+
+Ouvrir le fichier td1 decentralization.py 
+
+Exécuter le code
+
+## Auteur & contributeurs
+
+* **emeric99999** - *Travail initial* (https://github.com/emeric99999)
+  **YannPate** - *Contributeur* (https://github.com/YannPate)
+  **EPZ2003** - *Contributeur* (https://github.com/EPZ2003)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- td1 decentralization.py
+++ td1 decentralization.py
@@ -1,7 +1,72 @@
 error = "La valeur entrée est incorrecte\n"
-dicoFacile = {"allo ?" : ["oui ?", "non !", "feur...",1],"hello ?" : ["no","yes ?", "wut", 2 ]} 
-dicoMoyen = {"hello ?" : ["no","yes ?", "wut", 2 ]}
-dicoDifficile = {"hola ?" : ["nyet", "poyo", "si ?", 3]}
+dicoFacile = {
+"Quelle est la capitale de la France ?": ["Lyon", "Paris", "Marseille", 2],
+"Combien y a-t-il de continents dans le monde ?": ["8", "6", "7", 3],
+"Quel est le plus grand océan du monde ?": ["Océan Atlantique", "Océan Indien", "Océan Pacifique", 3],
+"Quel animal est connu pour sa lenteur extrême ?": ["Le paresseux", "Le guépard", "Le lapin", 1],
+"Quel élément chimique a pour symbole H ?": ["Hélium", "Hafnium", "Hydrogène", 3],
+"Dans quelle ville se trouve la Tour Eiffel ?": ["Paris", "Londres", "Rome", 1],
+"Quel est le fruit principal pour produire du vin ?": ["La pomme", "Le raisin", "La cerise", 2],
+"Quelle couleur obtient-on en mélangeant du bleu et du jaune ?": ["Rouge", "Orange", "Vert", 3],
+"Combien de jours compte une année non bissextile ?": ["366", "365", "364", 2],
+"Qui a écrit 'Les Misérables' ?": ["Émile Zola", "Honoré de Balzac", "Victor Hugo", 3],
+"Quel est l’instrument de musique typique de l’écosse ?": ["Cornemuse", "Guitare", "Violon", 1],
+"Combien de pattes ont les araignées ?": ["10", "6", "8", 3],
+"Quelle est la monnaie du Japon ?": ["Dollar", "Won", "Yen", 3],
+"Quel est le plus petit pays du monde ?": ["Vatican", "Monaco", "Saint-Marin", 1],
+"Quelle planète est connue comme la 'Planète Rouge' ?": ["Vénus", "Mars", "Jupiter", 2],
+"Quel légume est connu pour faire pleurer quand on le coupe ?": ["Oignon", "Pomme de terre", "Carotte", 1],
+"Quelle langue est parlée au Brésil ?": ["Espagnol", "Portugais", "Français", 2],
+"Dans quelle partie du corps se trouve le fémur ?": ["Bras", "Tête", "Jambe", 3],
+"Combien y a-t-il de couleurs dans un arc-en-ciel ?": ["6", "7", "8", 2],
+"Quel est le plus gros mammifère ?": ["Baleine bleue", "Éléphant", "Rhinocéros", 1]
+}
+
+dicoMoyen = {
+"Quel état des États-Unis est connu comme 'The Sunshine State' ?": ["Floride", "Californie", "Texas", 1],
+"Quelle est la plus grande île du monde ?": ["Groenland", "Madagascar", "Bornéo", 1],
+"Quel peintre est connu pour avoir été l'auteur de 'La Nuit étoilée' ?": ["Pablo Picasso", "Claude Monet", "Vincent van Gogh", 3],
+"En quelle année a eu lieu la chute du mur de Berlin ?": ["1990", "1989", "1985", 2],
+"Quel écrivain français a écrit 'Le Petit Prince' ?": ["Marcel Proust", "Antoine de Saint-Exupéry", "Jules Verne", 2],
+"Quelle est la capitale de l'Australie ?": ["Sydney", "Melbourne", "Canberra", 3],
+"Quel est le pays d'origine du tango ?": ["Argentine", "Espagne", "Italie", 1],
+"Quel est le plus long fleuve du monde ?": ["Amazone", "Nil", "Mississippi", 1],
+"Quel événement historique est associé au 14 juillet en France ?": ["La fin de la Première Guerre mondiale", "La prise de la Bastille", "La proclamation de la République", 2],
+"Qui a écrit 'L Étranger' ?": ["Jean-Paul Sartre", "Albert Camus", "André Gide", 2],
+"Quel pays est surnommé 'Le pays du soleil levant' ?": ["Chine", "Japon", "Corée", 2],
+"Dans quel océan se trouve l'île de Madagascar ?": ["Océan Indien", "Océan Pacifique", "Océan Atlantique", 1],
+"Qui a inventé l'ampoule électrique ?": ["Thomas Edison", "Nikola Tesla", "Alexander Bell", 1],
+"Quelle est la plus haute montagne du monde ?": ["K2", "Mont Blanc", "Everest", 3],
+"Dans quel pays peut-on visiter le Machu Picchu ?": ["Pérou", "Mexique", "Chili", 1],
+"Quel scientifique est célèbre pour la théorie de la relativité ?": ["Isaac Newton", "Galilée", "Albert Einstein", 3],
+"Quel est le symbole chimique de l'or ?": ["Ag", "Au", "Fe", 2],
+"Qui a peint la fresque de la Chapelle Sixtine ?": ["Raphaël", "Michel-Ange", "Léonard de Vinci", 2],
+"Quel écrivain a créé le personnage de Sherlock Holmes ?": ["Agatha Christie", "Edgar Allan Poe", "Arthur Conan Doyle", 3],
+"Quel est le plus grand désert du monde ?": ["Gobi", "Kalahari", "Sahara", 3]
+}
+
+dicoDifficile = {
+"Quel est le symbole chimique du tungstène ?": ["T", "Tu", "W", 3],
+"Qui a composé l'opéra 'La Flûte enchantée' ?": ["Ludwig van Beethoven", "Johann Sebastian Bach", "Wolfgang Amadeus Mozart", 3],
+"Quelle est la distance moyenne entre la Terre et la Lune ?": ["384 400 km", "150 000 km", "500 000 km", 1],
+"Quel pays a remporté la première Coupe du Monde de football en 1930 ?": ["Uruguay", "Brésil", "Italie", 1],
+"Quelle est la capitale de la Mongolie ?": ["Oulan-Bator", "Tachkent", "Astana", 1],
+"Quel écrivain a écrit 'Guerre et Paix' ?": ["Fiodor Dostoïevski", "Léon Tolstoï", "Alexandre Pouchkine", 2],
+"Quelle molécule est connue sous le nom de 'molécule de l'ADN' ?": ["Acide désoxyribonucléique", "Acide ribonucléique", "Acide citrique", 1],
+"Quel artiste est célèbre pour la fresque 'La Cène' ?": ["Michel-Ange", "Raphaël", "Léonard de Vinci", 3],
+"En quelle année Christophe Colomb a-t-il découvert l'Amérique ?": ["1482", "1492", "1502", 2],
+"Quel pays est le plus grand producteur de café au monde ?": ["Brésil", "Colombie", "Vietnam", 1],
+"Quelle est la profondeur approximative de la fosse des Mariannes ?": ["11 000 m", "8 000 m", "15 000 m", 1],
+"Quel chimiste a proposé le tableau périodique des éléments ?": ["Marie Curie", "Dimitri Mendeleïf", "John Dalton", 2],
+"Quel est le plus grand lac d'eau douce du monde ?": ["Lac Victoria", "Lac Baïkal", "Lac Supérieur", 3],
+"Quel philosophe grec est connu comme le précepteur d'Alexandre le Grand ?": ["Platon", "Socrate", "Aristote", 3],
+"Dans quel pays se trouve la vallée des rois ?": ["Égypte", "Grèce", "Turquie", 1],
+"Quelle est la vitesse de la lumière dans le vide ?": ["150 000 km/s", "299 792 km/s", "1 000 000 km/s", 2],
+"Quel mathématicien est connu pour son théorème sur les triangles rectangles ?": ["Euclide", "Archimède", "Pythagore", 3],
+"Qui a écrit 'Le Capital' ?": ["Adam Smith", "Friedrich Engels", "Karl Marx", 3],
+"Quelle galaxie est la plus proche de la Voie lactée ?": ["Andromède", "Petit Nuage de Magellan", "Grande Ourse", 1],
+"Quel est l'objectif principal de la technique 'Principal Component Analysis' (PCA) ?": ["Réduire la dimensionnalité des données.", "Augmenter le nombre de dimensions des données.", "Augmenter la variance des données.", 1]
+}
 
 def ChooseDifficulty():
     choix = input("Choisissez la difficulté\n1 : Facile\n2 : Moyen\n3 : Difficile\n")
